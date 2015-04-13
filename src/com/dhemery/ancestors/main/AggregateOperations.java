9ed2dfbaa5c29779927cb20018c878e44ad0f5b0
Dale Emery <dale@dhemery.com>
2015-04-13 13:12:49 -0700

    Rename Printer as Print

--- src/com/dhemery/ancestors/main/AggregateOperations.java
+++ src/com/dhemery/ancestors/main/AggregateOperations.java
@@ -5,13 +5,13 @@ import java.util.function.Consumer;
 import static com.dhemery.ancestors.gedcom.GedcomLoader.TREE;
 
 public class AggregateOperations {
-    private static final Consumer<Object> PRINT_PERSON = new Printer();
+    private static final Consumer<Object> PRINT = new Print();
 
     public static void main(String[] ignored) {
-        TREE.people().forEach(PRINT_PERSON);
+        TREE.people().forEach(PRINT);
     }
 
-    private static class Printer implements Consumer<Object> {
+    private static class Print implements Consumer<Object> {
         @Override
         public void accept(Object item) {
             System.out.println(item);
