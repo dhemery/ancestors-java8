751ce20235263bb22a945c3217272502bddb44c5
Dale Emery <dale@dhemery.com>
2015-04-21 20:26:44 -0700

    Remove Print class.

--- src/com/dhemery/ancestors/main/AggregateOperations.java
+++ src/com/dhemery/ancestors/main/AggregateOperations.java
@@ -1,20 +1,9 @@
 package com.dhemery.ancestors.main;
 
-import java.util.function.Consumer;
-
 import static com.dhemery.ancestors.gedcom.GedcomLoader.TREE;
 
 public class AggregateOperations {
-    private static final Consumer<Object> PRINT = new Print();
-
     public static void main(String[] ignored) {
-        TREE.people().forEach(PRINT);
-    }
-
-    private static class Print implements Consumer<Object> {
-        @Override
-        public void accept(Object item) {
-            System.out.println(item);
-        }
+        TREE.people().forEach(System.out::println);
     }
 }
